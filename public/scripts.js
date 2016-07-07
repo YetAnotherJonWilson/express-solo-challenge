2e7418fc2ba36ef6562cb9ae9c08dea1e1c8ab80
Jon Wilson <jonwilsoncomedy@gmail.com>
2016-07-07 15:00:23 -0500

    working plus animation

--- public/scripts.js
+++ public/scripts.js
@@ -1,19 +1,16 @@
 $('document').ready(function() {
+
   $('body').on('click', 'button', function() {
     $.get('/balance', function( data ) {
-      $( ".result" ).html( data );
-    });
-    // var something data.something (use postman to see this)
-    // div append var
+      $( ".result" ).html( data ).animate({
+    opacity: 1.3,
+    marginLeft: "0.6in",
+    fontSize: "10em",
+    borderWidth: "10px"
+  }, 1500 );
 
-
-
-
-
-//     $.get( "ajax/test.html", function( data ) {
-//   $( ".result" ).html( data );
-//   alert( "Load was performed." );
-// });
   });
+
+});
 
 });
